d1df58b9c3a2bac0a51bcd3ac1564d63b382a584
Jade Medeiros <jadesawandra@gmail.com>
2024-03-07 17:12:54 -0300

    pratica03

---
+++ teste.js
@@ -0,0 +1,23 @@
+{
+    "client": "Thunder Client",
+    "collectionName": "API REST teste",
+    "dateExported": "2024-03-07T20:10:49.557Z",
+    "version": "1.1",
+    "folders": [],
+    "requests": [
+        {
+            "_id": "a58d2c8b-c5c8-490b-adf6-6da59bedcc04",
+            "colId": "2878458c-bee4-442b-a79e-47c340f85afa",
+            "containerId": "",
+            "name": "https://dummyjson.com/products",
+            "url": "https://dummyjson.com/products/101",
+            "method": "DELETE",
+            "sortNum": 10000,
+            "created": "2024-03-07T20:09:59.972Z",
+            "modified": "2024-03-07T20:09:59.972Z",
+            "headers": [],
+            "params": [],
+            "tests": []
+        }
+    ]
+}
